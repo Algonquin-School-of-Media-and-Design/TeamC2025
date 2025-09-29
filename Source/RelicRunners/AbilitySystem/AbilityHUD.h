696d5610e5704a2805b538350231845550bea557
AxxL28 <leo00011@algonquinlive.com>
2025-09-29 14:36:00 -0400

    Added widgets for ability system

---
+++ Source/RelicRunners/AbilitySystem/AbilityHUD.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AbilityHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RELICRUNNERS_API UAbilityHUD : public UUserWidget
+{
+	GENERATED_BODY()
+
+
+	
+};
---
+++ Source/RelicRunners/AbilitySystem/AbilityPointWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AbilityPointWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RELICRUNNERS_API UAbilityPointWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
