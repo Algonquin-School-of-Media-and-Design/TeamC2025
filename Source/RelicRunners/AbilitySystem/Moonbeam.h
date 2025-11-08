ef2084ce7134d71fd6194d32cdec900dbcb85873
Ali Mahdi <dgalim@hotmail.com>
2025-11-07 19:15:09 -0500

    Fixed push problem

---
+++ Source/RelicRunners/AbilitySystem/Moonbeam.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilityBase.h"
+#include "Moonbeam.generated.h"
+
+UCLASS()
+class RELICRUNNERS_API AMoonbeam : public AAbilityBase
+{
+    GENERATED_BODY()
+
+public:
+    AMoonbeam();
+
+    virtual void ActivateAbility() override;
+    virtual void EndAbility() override;
+    virtual bool CanActivate() const override;
+
+protected:
+    // How far the beam goes
+    UPROPERTY(EditDefaultsOnly, Category = "Moonbeam")
+    float BeamRange;
+
+    // Visual thickness for debug 
+    UPROPERTY(EditDefaultsOnly, Category = "Moonbeam")
+    float BeamRadius;
+
+    // How long the debug beam stays visible
+    UPROPERTY(EditDefaultsOnly, Category = "Moonbeam")
+    float BeamDuration;
+
+    // Internal cooldown timer for this ability
+    FTimerHandle CooldownTimerHandle;
+
+    void ResetCooldown();
+    void SpawnBeamEffect(const FVector& Start, const FVector& End);
+};
