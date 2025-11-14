fce26b684c0447c3d6f0cf068899fc53e33df236
Ali Mahdi <dgalim@hotmail.com>
2025-11-13 19:25:16 -0500

    Polished Moonbeam.cpp

--- Source/RelicRunners/AbilitySystem/Moonbeam.cpp
+++ Source/RelicRunners/AbilitySystem/Moonbeam.cpp
@@ -2,7 +2,7 @@
 
 #include "GameFramework/Character.h"
 #include "GameFramework/Controller.h"
-#include "DrawDebugHelpers.h"
+#include "DrawDebugHelpers.h" 
 #include "TimerManager.h"
 #include "Kismet/GameplayStatics.h"
 
@@ -10,26 +10,20 @@
 #include "RelicRunners/Enemy/EnemyCharacterAI.h"
 
 
-
 AMoonbeam::AMoonbeam()
-{
-    // Identity
+{ 
     AbilityName = FName(TEXT("Moonbeam"));
-    AbilityCategory = EAbilityCategory::Offensive;
-    TargetType = EAbilityTargetType::Cone; 
-
+    
     // Tuning
     DamageAmount = 20.f;      
-    Cooldown = 5.f;       
-    Duration = 5.0f;       
+    Cooldown = 5.f;         
 
-    // Beam visuals
+    // Beam visuals (will be adding more detailed visuals later)
     BeamRange = 1500.f;
     BeamRadius = 6.f;
     BeamDuration = 5.0f;
 
     bIsOnCooldown = false;
-    bCanBeInterrupted = true;
 }
 
 bool AMoonbeam::CanActivate() const
@@ -43,22 +37,9 @@ void AMoonbeam::ActivateAbility()
     {
         return;
     }
-
-    if (!CanActivate())
-    {
-        UE_LOG(LogTemp, Verbose, TEXT("Moonbeam: Tried to cast while on cooldown."));
-        return;
-    }
-
-    
+   
     AActor* LocalOwner = OwnerActor ? OwnerActor : GetOwner();
     ACharacter* OwnerChar = Cast<ACharacter>(LocalOwner);
-
-    if (!OwnerChar)
-    {
-        UE_LOG(LogTemp, Warning, TEXT("Moonbeam: No valid character owner."));
-        return;
-    }
 
     FVector Start;
     FRotator ViewRot;
@@ -77,19 +58,15 @@ void AMoonbeam::ActivateAbility()
 
     SpawnBeamEffect(Start, End);
 
-    // DAMAGE (server authoritative, piercing)
     if (OwnerChar->HasAuthority())
     {
         FCollisionQueryParams Params(SCENE_QUERY_STAT(MoonbeamTrace), false);
         Params.AddIgnoredActor(OwnerChar);
         Params.AddIgnoredActor(this);
 
-        TSet<AActor*> HitActors;
-
         const FVector TraceDir = (End - Start).GetSafeNormal();
         FVector TraceStart = Start;
         float RemainingDistance = BeamRange;
-        const float Epsilon = 10.f; // step past each hit so we don't get stuck
 
         while (RemainingDistance > 0.f)
         {
@@ -98,7 +75,7 @@ void AMoonbeam::ActivateAbility()
 
             if (!GetWorld()->LineTraceSingleByChannel(Hit, TraceStart, TraceEnd, ECC_Pawn, Params))
             {
-                break; // no more hits along the rest of the beam
+                break; 
             }
 
             AActor* HitActor = Hit.GetActor();
@@ -107,52 +84,26 @@ void AMoonbeam::ActivateAbility()
                 break;
             }
 
-            // Only damage your enemies
-            if (Cast<AEnemyCharacterAI>(HitActor) || Cast<AEnemyCharacter>(HitActor))
+            if (HitActor && (HitActor->IsA<AEnemyCharacterAI>() || HitActor->IsA<AEnemyCharacter>()))
             {
-                if (!HitActors.Contains(HitActor))
-                {
-                    HitActors.Add(HitActor);
-
-                    UGameplayStatics::ApplyDamage(
-                        HitActor,
-                        DamageAmount,
-                        OwnerChar->GetController(),
-                        OwnerChar,   // DamageCauser
-                        nullptr
-                    );
-
-                    UE_LOG(LogTemp, Log, TEXT("Moonbeam pierced %s for %f damage"),
-                        *HitActor->GetName(), DamageAmount);
-                }
+                UGameplayStatics::ApplyDamage(HitActor, DamageAmount, OwnerChar->GetController(), OwnerChar, nullptr);
             }
 
-            Params.AddIgnoredActor(HitActor);
-
-            // Advance start a bit past this hit
-            const float Traveled = (Hit.ImpactPoint - TraceStart).Size();
-            RemainingDistance -= (Traveled + Epsilon);
-            TraceStart = Hit.ImpactPoint + TraceDir * Epsilon;
+            Params.AddIgnoredActor(HitActor);           
         }
     }
 
-    // === COOLDOWN ===
+    // This part might change later becuase I know antonio is making changes in the ability system so will see
     bIsOnCooldown = true;
     if (Cooldown > 0.f)
     {
-        GetWorld()->GetTimerManager().SetTimer(
-            CooldownTimerHandle,
-            this,
-            &AMoonbeam::ResetCooldown,
-            Cooldown,
-            false
-        );
+        GetWorld()->GetTimerManager().SetTimer(CooldownTimerHandle,this,&AMoonbeam::ResetCooldown,Cooldown,false);
     }
 }
 
 void AMoonbeam::EndAbility()
 {
-    // Not used yet.
+    // This is for later when adding for example some animition when make it end here and the effects end here too
 }
 
 void AMoonbeam::ResetCooldown()
@@ -167,14 +118,9 @@ void AMoonbeam::SpawnBeamEffect(const FVector& Start, const FVector& End)
         return;
     }
 
-    DrawDebugLine(
-        GetWorld(),
-        Start,
-        End,
-        FColor::Cyan,
-        false,
-        BeamDuration,
-        0,
-        BeamRadius
-    );
+    // This line is to controll the visual of the beam that shows when pressing the ability button. For now it's just a debug line but later it will have more functionality
+    // These values can be changed at the top 
+    // I used this by adding DrawDebugHelpers.h (great library)
+    DrawDebugLine(GetWorld(),Start,End,FColor::Cyan,false,BeamDuration,0,BeamRadius);
+    
 }
