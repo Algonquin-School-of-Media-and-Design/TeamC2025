3dd550a084ea3917aad4e2a7e225eeb60af8b5b6
justingit99 <quinnjustin99@gmail.com>
2025-11-21 14:09:30 -0500

    Fixed issue when initalizing too many inventory items

--- Source/RelicRunners/RelicRunnersCharacter.cpp
+++ Source/RelicRunners/RelicRunnersCharacter.cpp
@@ -633,7 +633,7 @@ void ARelicRunnersCharacter::SpawnStarterItems()
 {
 	if (ItemMeshData && InventoryComponent)
 	{
-		for (int i = 0; i < 1000; ++i)
+		for (int i = 0; i < 50; ++i)
 		{
 			UItemObject* Item = ItemStats::CreateItemFromData(ItemStats::CreateSpecificItemData(1, ItemStats::GetRandomItemType(), ItemMeshData), InventoryComponent);
 			InventoryComponent->AddItem(Item);
